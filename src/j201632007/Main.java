417680d7d69908ed5b1792a41bd0d73a2c215953
Heexoo <hisu138@gmail.com>
2022-10-24 16:42:25 +0900

    Main.java 수정1

--- src/j201632007/Main.java
+++ src/j201632007/Main.java
@@ -3,8 +3,8 @@ package j201632007;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s = "hello world"; 
+		System.out.println(s);
 	}
 
 }
